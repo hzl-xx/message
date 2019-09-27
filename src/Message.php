27a5de7a130cc71f7894cae326a9d93c62217577
黄振 <847621843@qq.com>
2019-09-27 15:55:46 +0800

    新增http

--- src/Message.php
+++ src/Message.php
@@ -2,6 +2,7 @@
 namespace Aiqbg\Message;
 
 use Grpc\ChannelCredentials;
+use GuzzleHttp\Client;
 use Rpc\Auth\AuthClient;
 use Rpc\Auth\Key;
 use Rpc\Message\Common;
@@ -15,6 +16,7 @@ class Message
     {
         $this->config = $config->get("messageserver");
     }
+
 
     public function initCli($clientName)
     {
@@ -76,5 +78,35 @@ class Message
         $req->setSend($user);
         $req->setSendGroup($group);
         return $this->simpleRequest("Rpc\Message\SendMessageServiceClient", "SendMessage", $req);
+    }
+
+    /**
+     *
+     *User:
+     *DateTime:
+     * @param string $group
+     * @param $title
+     * @param $message
+     * @param $receive
+     * @param string $send 以英文逗号组装，比如XiaoXiongWei,HuangZhen
+     */
+    function sendMessageHttp($title,$message,$user='',$group='',$receive='')
+    {
+        $data['title']=$title;
+        $data['message']=$message;
+        $data['receive']=$receive ? : $this->config["receive"];
+        $data['send']=($user == "" && $group == "")?$this->config["group_strategy"]["default"] : $user;
+        $data['send_group']=$group;
+        $token=app(Client::class)->get($this->config['http_host'].'/auth?key='.$this->getKey())->getBody()->getContents();
+        $token = json_decode($token, true)['access_token'];
+
+        $query = json_encode(array_filter($data));
+        $result = app(Client::class)->post($this->config['http_host'].'/v1/send/message', [
+            'body' => $query,
+            'headers'=>[
+                'token'=>$token
+            ]
+        ])->getBody()->getContents();
+        return $result;
     }
 }
